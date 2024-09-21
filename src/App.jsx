2ebf7f940daa64b3217bc37cc804c0f7b8bb6a27
Oludire Damilare <oludire@gmail.com>
2024-09-21 09:13:03 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl bg-red-500">
+      "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+    </h1>
+  );
+}
