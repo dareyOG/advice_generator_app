0ddd0d4f991dfa26ef4701a9cffeaaa38b370864
Oludire Damilare <oludire@gmail.com>
2024-09-23 09:18:39 +0100

    Update Advice.jsx

--- src/component/Advice.jsx
+++ src/component/Advice.jsx
@@ -2,7 +2,7 @@ function Advice({ advice }) {
   const { advice: new_advice, id } = advice;
   return (
     <section className=" text-center ">
-      <h1 className="text-primary">Advice #{id}</h1>
+      <h1 className="text-primary text-[0.5rem]">Advice #{id}</h1>
       <blockquote className="text-primary-light py-4 text-[1.5rem] md:text-[3rem]">
         {new_advice}
       </blockquote>
